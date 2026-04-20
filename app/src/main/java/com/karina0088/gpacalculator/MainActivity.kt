9eb28ea2795bd28a8be7a9ac92060218b1e3143f
karinanurarieff <karinaarieffin68138@gmail.com>
2026-04-20 16:26:17 +0700

    Initial project setup

--- app/src/main/java/com/karina0088/gpacalculator/MainActivity.kt
+++ app/src/main/java/com/karina0088/gpacalculator/MainActivity.kt
@@ -3,28 +3,45 @@ package com.karina0088.gpacalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.karina0088.gpacalculator.ui.theme.GPACalculatorTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            AppNav()
+            GPACalculatorTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
     }
 }
 
 @Composable
-fun AppNav() {
-    val navController = rememberNavController()
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-    NavHost(navController, startDestination = "home") {
-        composable("home") {
-            HomeScreen(navController)
-        }
-        composable("result/{nilai}") {
-            ResultScreen(navController, "0")
-        }
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GPACalculatorTheme {
+        Greeting("Android")
     }
 }
